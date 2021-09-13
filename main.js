3e3945bb214aeea00556b1e636242dde39bdb445
robinkelsickii <robinkelsickii@gmail.com>
2021-09-12 22:48:57 -0400

    added a double digit function that makes numbers les than 10 appear with a 0 in front

--- main.js
+++ main.js
@@ -4,10 +4,18 @@ var displayTime = document.getElementById('clock');
 function currentTime() {
     let time = new Date;
 
-    let hours = time.getHours();
-    let minutes = time.getMinutes();
-    let seconds = time.getSeconds();
+    let hours = doubleDigits(time.getHours());
+    let minutes = doubleDigits(time.getMinutes());
+    let seconds = doubleDigits(time.getSeconds());
 
     displayTime.innerText = `${hours} : ${minutes} : ${seconds}`
 }
+
+function doubleDigits(time) {
+    if (time < 10) {
+        return '0' + time;
+    }
+    return time;
+}
+
 setInterval(currentTime, 1000);
